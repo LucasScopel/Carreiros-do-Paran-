7d46ac02ba1710f4460c75e5be1425d21073761e
Henrique Braga <henriquesb90s@gmail.com>
2026-06-19 13:13:23 -0300

    Agora permite enviar imagens

--- apps/web/app/create-trail/page.tsx
+++ apps/web/app/create-trail/page.tsx
@@ -2,7 +2,7 @@ import CreateTrailForm from "./components/create-trail-form";
 
 export default function CreateTrailPage() {
   return (
-    <div className="flex w-full min-h-full bg-slate-50 flex justify-center items-center">
+    <div className="w-full min-h-screen bg-slate-50 flex justify-center items-center px-4 py-8">
       <CreateTrailForm />
     </div>
   );
