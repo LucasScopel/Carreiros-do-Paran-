29d701640940c32d7a17266447035c2ce3710287
Henrique Braga <henriquesb90s@gmail.com>
2026-06-22 22:43:25 -0300

    Criação das paginas se sugestão de novas trilhas

---
+++ apps/web/app/suggest-trails/admin/page.tsx
@@ -0,0 +1,9 @@
+import SuggestTrailsAdmin from "./components";
+
+export default function AdminSuggestTrailsPage() {
+  return (
+    <div className="w-full min-h-screen bg-slate-50 px-4 py-8">
+      <SuggestTrailsAdmin />
+    </div>
+  );
+}
---
+++ apps/web/app/suggest-trails/hikers/page.tsx
@@ -0,0 +1,9 @@
+import SuggestTrailForm from "./components";
+
+export default function SuggestTrailsPage() {
+  return (
+    <div className="w-full min-h-screen bg-slate-50 px-4 py-8">
+      <SuggestTrailForm />
+    </div>
+  );
+}
