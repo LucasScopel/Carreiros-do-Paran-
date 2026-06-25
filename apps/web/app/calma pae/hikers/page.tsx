b946b5c532277da8334bc3ccd780232e3e10cdf5
Henrique Braga <henriquesb90s@gmail.com>
2026-06-25 11:15:46 -0300

    Preparação para a página de admins de sugestões de trilha

--- apps/web/app/calma pae/hikers/page.tsx
+++
@@ -1,9 +0,0 @@
-import SuggestTrailForm from "./components";
-
-export default function SuggestTrailsPage() {
-  return (
-    <div className="w-full min-h-screen bg-slate-50 px-4 py-8">
-      <SuggestTrailForm />
-    </div>
-  );
-}
