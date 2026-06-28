fb1302384ee2acc0a7c94214ff94885cbcd0c260
GustavoLank <64474984+GustavoLank@users.noreply.github.com>
2026-06-28 00:29:37 -0300

    Adiciona um sub comando utilitário para rapidamente criar usuários de teste durante o desenvolvimento

--- scripts/users.ts
+++ scripts/users.ts
@@ -1,11 +1,13 @@
 import { prisma } from "../packages/database";
+import argon2 from "argon2";
+import { nanoid } from "nanoid";
 
 const COMMANDS: Record<string, () => Promise<void>> = {
   "grant-admin": async () => {
     const email = process.argv[3];
 
     if (!email) {
-      console.error("Usage: pnpm grant-admin <email>");
+      console.error("Usage: pnpm user grant-admin <email>");
       process.exit(1);
     }
 
@@ -25,7 +27,7 @@ const COMMANDS: Record<string, () => Promise<void>> = {
     const email = process.argv[3];
 
     if (!email) {
-      console.error("Usage: pnpm revoke-admin <email>");
+      console.error("Usage: pnpm user revoke-admin <email>");
       process.exit(1);
     }
 
@@ -39,6 +41,81 @@ const COMMANDS: Record<string, () => Promise<void>> = {
     });
 
     console.log(`${email} is not an administrator anymore.`);
+  },
+
+  create: async () => {
+    const args = process.argv.slice(3);
+
+    const admin = args.includes("--admin");
+    const verified = args.includes("--verified");
+
+    const positional = args.filter((arg) => !arg.startsWith("--"));
+
+    let email: string;
+    let password: string;
+    let name: string;
+
+    if (positional.length === 1) {
+      name = positional[0];
+
+      email = `${name}@example.com`;
+      password = "123456";
+    } else if (positional.length === 2) {
+      email = positional[0];
+      password = positional[1];
+
+      name = email.split("@")[0];
+    } else {
+      console.error(
+        `Usage:\n\n  pnpm create-user <name> [--admin] [--verified]\n\nor\n\n  pnpm create-user <email> <password> [--admin] [--verified]\n`,
+      );
+
+      process.exit(1);
+    }
+
+    const hash = await argon2.hash(password);
+
+    const publicId = nanoid();
+
+    const user = await prisma.$transaction(async (tx) => {
+      const user = await tx.user.create({
+        data: {
+          publicId,
+          email,
+          name,
+          password: hash,
+          birthDate: new Date("2000-01-01"),
+          emailVerified: verified,
+          admin: admin,
+        },
+        select: {
+          id: true,
+          publicId: true,
+          name: true,
+          email: true,
+          admin: true,
+          emailVerified: true,
+        },
+      });
+
+      const collectionPublicId = nanoid();
+
+      await tx.trailCollection.create({
+        data: {
+          publicId: collectionPublicId,
+          name: "Salvas",
+          isDefault: true,
+          userId: user.id,
+        },
+      });
+
+      return user;
+    });
+
+    console.log({
+      ...user,
+      password,
+    });
   },
 };
 
