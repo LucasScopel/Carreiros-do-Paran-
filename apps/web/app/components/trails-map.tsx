d20018a520a7ea982e74a98fb5c1cd0e08da2a3a
GustavoLank <64474984+GustavoLank@users.noreply.github.com>
2026-07-04 11:54:30 -0300

    Configuração do Martin e implementação inicial da página principal

---
+++ apps/web/app/components/trails-map.tsx
@@ -0,0 +1,380 @@
+"use client";
+
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import maplibregl, { LngLatBounds, VectorTileSource } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+const PARANA_BOUNDS = [
+  [-54.620889, -26.716667], // Oeste, Sul
+  [-48.023611, -22.516111], // Leste, Norte
+];
+
+interface Props {
+  filters: {
+    difficulty: number | null;
+    minLength: number;
+    maxLength: number;
+    minDuration: number;
+    maxDuration: number;
+    minRating?: number;
+  };
+  setBbox: (bounds: LngLatBounds) => void;
+}
+
+export interface MapRef {
+  flyTo: (lng: number, lat: number, zoom?: number) => void;
+}
+
+function buildTilesUrl(filters: Props["filters"]) {
+  const params = new URLSearchParams();
+
+  if (filters.difficulty !== null)
+    params.append("difficulty", filters.difficulty.toString());
+
+  params.append("min_length", filters.minLength.toString());
+  params.append("max_length", filters.maxLength.toString());
+
+  params.append("min_duration", filters.minDuration.toString());
+  params.append("max_duration", filters.maxDuration.toString());
+
+  return `http://localhost:8081/tiles/trails_map_layer/{z}/{x}/{y}?${params.toString()}`;
+}
+
+// eslint-disable-next-line react/display-name
+export const TrailMap = forwardRef<MapRef, Props>(
+  ({ filters, setBbox }, ref) => {
+    const mapContainer = useRef<HTMLDivElement>(null);
+    const mapRef = useRef<maplibregl.Map>(null);
+
+    useImperativeHandle(ref, () => ({
+      flyTo(lng: number, lat: number, zoom = 14) {
+        if (mapRef.current) {
+          mapRef.current.flyTo({
+            center: [lng, lat],
+            zoom: zoom,
+            essential: true,
+            speed: 1.5,
+            curve: 1.2,
+          });
+        }
+      },
+
+      setTrailHoverStatus(id: number, hovered: boolean) {
+        if (mapRef.current) {
+          mapRef.current.setFeatureState(
+            {
+              source: "trails",
+              sourceLayer: "trails_map_layer",
+              id: id,
+            },
+            { hover: hovered },
+          );
+        }
+      },
+    }));
+
+    useEffect(() => {
+      if (!mapRef.current) return;
+
+      const source = mapRef.current.getSource<VectorTileSource>("trails");
+
+      if (source) {
+        source.setTiles([buildTilesUrl(filters)]);
+      }
+    }, [filters]);
+
+    useEffect(() => {
+      if (!mapContainer.current) {
+        return;
+      }
+
+      const [[west, south], [east, north]] = PARANA_BOUNDS;
+
+      const map = new maplibregl.Map({
+        container: mapContainer.current,
+
+        style: {
+          version: 8,
+          sources: {
+            osm: {
+              type: "raster",
+              tiles: [
+                "https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png",
+              ],
+              tileSize: 256,
+              attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            },
+          },
+          layers: [
+            {
+              id: "osm",
+              type: "raster",
+              source: "osm",
+            },
+          ],
+        },
+
+        center: [(west + east) / 2, (south + north) / 2],
+        zoom: 6.5,
+        minZoom: 1,
+        maxZoom: 20,
+      });
+
+      mapRef.current = map;
+
+      map.addControl(new maplibregl.NavigationControl());
+
+      map.on("load", () => {
+        // Limita o mapa apenas ao Paraná mas com uma certa margem de liberdade
+        /* const lonMargin = east - west;
+      const latMargin = (north - south) * 0.5;
+
+      map.setMaxBounds([
+        [west - lonMargin, south - latMargin],
+        [east + lonMargin, north + latMargin],
+      ]); */
+
+        // Adiciona a fonte de dados de trilhas
+        map.addSource("trails", {
+          type: "vector",
+          tiles: [buildTilesUrl(filters)],
+          promoteId: "id",
+          minzoom: 0,
+          maxzoom: 18,
+        });
+
+        // Adiciona a fonte da máscara invertida do Paraná
+        map.addSource("parana-mask", {
+          type: "geojson",
+          data: "/parana-mask.geojson",
+        });
+
+        // Adiciona uma camada que escurece todo o mapa exceto o Paraná
+        // Usa a máscara adicionada acima
+        map.addLayer({
+          id: "parana-mask",
+          type: "fill",
+          source: "parana-mask",
+          paint: {
+            "fill-color": "#000000",
+            "fill-opacity": 0.2,
+          },
+        });
+
+        // Camada dos círculos dos clusters
+        map.addLayer({
+          id: "clusters-circles",
+          source: "trails",
+          "source-layer": "trails_map_layer",
+          type: "circle",
+          filter: [">", ["get", "trail_count"], 1],
+          paint: {
+            "circle-color": [
+              "case",
+              ["boolean", ["feature-state", "hover"], false],
+              "#00998a",
+              "#005C53",
+            ],
+            "circle-stroke-color": "#ffffff",
+            "circle-radius": [
+              "*",
+              ["case", ["boolean", ["feature-state", "hover"], false], 1.25, 1],
+              ["step", ["get", "trail_count"], 18, 10, 24, 50, 32, 100, 40],
+            ],
+            "circle-stroke-width": [
+              "case",
+              ["boolean", ["feature-state", "hover"], false],
+              3,
+              2,
+            ],
+          },
+        });
+
+        // Camada dos contadores dos clusters
+        map.addLayer({
+          id: "clusters-counters",
+          source: "trails",
+          "source-layer": "trails_map_layer",
+          type: "symbol",
+          filter: [">", ["get", "trail_count"], 1],
+          layout: {
+            "text-field": ["to-string", ["get", "trail_count"]],
+            "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
+            "text-size": 14,
+            "text-justify": "auto",
+          },
+          paint: {
+            "text-color": "#ffffff",
+          },
+        });
+
+        // Camada dos círculos das trilhas
+        map.addLayer({
+          id: "trails-circles",
+          source: "trails",
+          "source-layer": "trails_map_layer",
+          type: "circle",
+          filter: ["==", ["get", "trail_count"], 1],
+          paint: {
+            "circle-radius": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              5,
+              [
+                "*",
+                5,
+                ["case", ["boolean", ["feature-state", "hover"], false], 2, 1],
+              ],
+              16,
+              [
+                "*",
+                20,
+                ["case", ["boolean", ["feature-state", "hover"], false], 2, 1],
+              ],
+            ],
+            "circle-stroke-width": [
+              "*",
+              1.5,
+              ["case", ["boolean", ["feature-state", "hover"], false], 2, 1],
+            ],
+            "circle-stroke-color": "#bb763e",
+            "circle-color": [
+              "case",
+              ["boolean", ["feature-state", "hover"], false],
+              "#eb9b59",
+              "#D99C6A",
+            ],
+          },
+        });
+
+        // Camada dos nomes das trilhas
+        map.addLayer({
+          id: "trails-names",
+          source: "trails",
+          "source-layer": "trails_map_layer",
+          type: "symbol",
+          filter: ["==", ["get", "trail_count"], 1],
+          layout: {
+            "text-field": ["get", "name"],
+            "text-font": ["Open Sans Regular", "Arial Unicode MS Regular"],
+            "text-size": 11,
+            "text-offset": [0, 1.2],
+            "text-anchor": "top",
+            "text-variable-anchor": ["top", "bottom", "left", "right"],
+            "text-max-width": 10,
+          },
+          paint: {
+            "text-color": "#222222",
+            "text-halo-color": "#ffffff",
+            "text-halo-width": 1.5,
+          },
+        });
+      });
+
+      map.on("moveend", () => setBbox(map.getBounds()));
+
+      map.on("click", "trails-circles", (e) => {
+        const feature = e.features?.[0];
+
+        if (!feature) {
+          return;
+        }
+
+        console.log(feature.properties);
+      });
+
+      map.on("click", "clusters-circles", (e) => {
+        const feature = e.features?.[0];
+        if (!feature) return;
+
+        map.flyTo({
+          center: feature.geometry.coordinates,
+          zoom: map.getZoom() + 2.5,
+          essential: true,
+          speed: 1.2,
+          curve: 1.4,
+        });
+      });
+
+      let hoveredTrailId: number | null = null;
+
+      function onMouseEnter(
+        e: maplibregl.MapMouseEvent & {
+          features?: maplibregl.MapGeoJSONFeature[];
+        },
+      ) {
+        const feature = e.features?.[0];
+        if (!feature) return;
+
+        if (hoveredTrailId !== null) {
+          map.setFeatureState(
+            {
+              source: "trails",
+              sourceLayer: "trails_map_layer",
+              id: hoveredTrailId,
+            },
+            { hover: false },
+          );
+        }
+
+        hoveredTrailId = feature.id as number;
+        map.setFeatureState(
+          {
+            source: "trails",
+            sourceLayer: "trails_map_layer",
+            id: hoveredTrailId,
+          },
+          { hover: true },
+        );
+
+        map.getCanvas().style.cursor = "pointer";
+      }
+
+      function onMouseLeave() {
+        if (hoveredTrailId !== null) {
+          map.setFeatureState(
+            {
+              source: "trails",
+              sourceLayer: "trails_map_layer",
+              id: hoveredTrailId,
+            },
+            { hover: false },
+          );
+        }
+        hoveredTrailId = null;
+        map.getCanvas().style.cursor = "";
+      }
+
+      map.on("mouseenter", "trails-circles", onMouseEnter);
+      map.on("mouseenter", "clusters-circles", onMouseEnter);
+
+      map.on("mouseleave", "trails-circles", onMouseLeave);
+      map.on("mouseleave", "clusters-circles", onMouseLeave);
+
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "z") {
+          console.log(map.getZoom());
+          console.log(
+            map
+              .queryRenderedFeatures({ layers: ["trails-circles"] })
+              .map((x) => x.properties),
+          );
+        }
+      });
+
+      return () => {
+        map.remove();
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+      <div
+        ref={mapContainer}
+        className="h-full w-full focus:outline-none cursor:none"
+      />
+    );
+  },
+);
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,175 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { MapRef, TrailMap } from "./components/trails-map";
+import { LocateFixed } from "lucide-react";
+import { toast } from "sonner";
+import DoubleRangeSlider from "./components/double-range-slider";
+import { useDebounce } from "@/hooks/useDebounce";
+
 export default function Home() {
+  const mapRef = useRef<MapRef>(null);
+
+  const [bbox, setBbox] = useState<number[] | null>(null);
+  const [ordering, setOrdering] = useState("");
+  const [filters, setFilters] = useState({
+    difficulty: null as number | null,
+    minLength: 0,
+    maxLength: 30,
+    minDuration: 0,
+    maxDuration: 300,
+  });
+  const debouncedFilters = useDebounce(filters, 400);
+
+  function handleShowMyLocation() {
+    if (!navigator.geolocation) {
+      toast.error("Geolocalização não é suportada pelo seu navegador");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        mapRef.current?.flyTo(
+          position.coords.longitude,
+          position.coords.latitude,
+        );
+      },
+      () => toast.error("Não foi possível identificar sua localização"),
+    );
+  }
+
+  function handleDifficultyChange(value: number) {
+    setFilters((prev) => ({
+      ...prev,
+      difficulty: value === 0 ? null : value,
+    }));
+  }
+
+  function handleLengthRangeChange({ min, max }: { min: number; max: number }) {
+    setFilters((prev) => ({
+      ...prev,
+      minLength: min,
+      maxLength: max,
+    }));
+  }
+
+  function handleDurationRangeChange({
+    min,
+    max,
+  }: {
+    min: number;
+    max: number;
+  }) {
+    setFilters((prev) => ({
+      ...prev,
+      minDuration: min,
+      maxDuration: max,
+    }));
+  }
+
+  function handleOrderingChange(value: string) {
+    setOrdering(value);
+  }
+
   return (
-    <div>
-      <main>
-      </main>
+    <div className="flex w-full min-h-full items-center justify-center bg-cover bg-center">
+      <aside className="sticky relative top-0 left-0 z-40 shrink-0 w-96 h-full">
+        <h2 className="m-4 text-xl font-semibold">Explorar trilhas</h2>
+
+        <div className="space-y-4 border-b p-4">
+          <div>
+            <label className="mb-1 block text-sm font-medium">
+              Dificuldade
+            </label>
+
+            <select
+              onChange={(e) => handleDifficultyChange(Number(e.target.value))}
+              className="w-full rounded-md border p-2 text-sm"
+            >
+              <option value="0">Todas</option>
+              <option value="1">Muito fácil</option>
+              <option value="2">Fácil</option>
+              <option value="3">Média</option>
+              <option value="4">Difícil</option>
+            </select>
+          </div>
+
+          <div>
+            <label className="mb-1 block text-sm font-medium">
+              Tamanho do trajeto
+            </label>
+
+            <DoubleRangeSlider
+              min={0}
+              max={30}
+              suffix="km"
+              onChange={handleLengthRangeChange}
+            />
+          </div>
+
+          <div>
+            <label className="mb-1 block text-sm font-medium">
+              Duração do trajeto
+            </label>
+
+            <DoubleRangeSlider
+              min={0}
+              max={300}
+              step={10}
+              suffix="min"
+              onChange={handleDurationRangeChange}
+            />
+          </div>
+
+          <div>
+            <label className="mb-1 block text-sm font-medium">
+              Ordenar por
+            </label>
+
+            <select
+              onChange={(e) => handleOrderingChange(e.target.value)}
+              className="w-full rounded-md border p-2 text-sm"
+            >
+              <option value="highest-rating">Avaliação mais alta</option>
+              <option value="lowest-rating">Avaliação mais baixa</option>
+            </select>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          onClick={handleShowMyLocation}
+          className="absolute -right-14 bottom-0 w-10 h-10 p-2 bg-white text-black rounded-full m-2 cursor-pointer group hover:bg-gray-300 transition-colors duration-300"
+        >
+          <LocateFixed />
+          <div
+            className={`
+              absolute top-1/2 -translate-y-1/2
+              whitespace-nowrap
+              rounded-md px-2 py-1
+              text-sm text-white
+              bg-black/70 opacity-0
+              pointer-events-none transition-all group-hover:opacity-100
+              left-0 translate-x-12 group-hover:translate-x-14
+            `}
+          >
+            Mostrar minha localização
+          </div>
+        </button>
+      </aside>
+
+      <TrailMap
+        ref={mapRef}
+        filters={debouncedFilters}
+        setBbox={(bounds) =>
+          setBbox([
+            bounds.getWest(),
+            bounds.getSouth(),
+            bounds.getEast(),
+            bounds.getNorth(),
+          ])
+        }
+      />
     </div>
   );
 }
